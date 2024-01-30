bd192b2ef5df5053668cce3935bf1a68bbfa437a
hoagminh <hoagminhit@gmail.com>
2024-01-30 16:31:34 +0700

    chapter 5

---
+++ app/admin/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Customer() {
+  return <div>Customers Page</div>;
+}
+
+export default Customer;
---
+++ app/admin/invoices/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Invoice() {
+  return <div>Invoices Page</div>;
+}
+
+export default Invoice;
